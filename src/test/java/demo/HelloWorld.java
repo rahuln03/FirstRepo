14a8eaa42439719225cf1ae5e7b63f5f572a3eff
Lenovo <Lenovo@DESKTOP-K3KEONS>
2023-09-27 00:13:10 +0530

    First Commit

---
+++ src/test/java/demo/HelloWorld.java
@@ -0,0 +1,16 @@
+package demo;
+
+public class HelloWorld 
+{
+public static void main(String[] args) 
+
+{
+System.out.println("Hello Rahul");	
+System.out.println("......................");
+System.out.println("Hello Rahul...and all");
+
+}
+
+
+
+}
